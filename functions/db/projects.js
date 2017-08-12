3cadf3b5f37f1fdb140a1f7f18f6fcd3355c56d6
Nathan Gaberel <nathang@theodo.co.uk>
2017-08-12 01:06:44 +0100

    Add defect collector.

---
+++ functions/db/projects.js
@@ -0,0 +1,60 @@
+const admin = require('firebase-admin')
+
+function isTracked (boardId) {
+  return admin
+    .database()
+    .ref(`projects/${boardId}`)
+    .once('value')
+    .then(snapshot => snapshot.exists())
+}
+
+function createOrUpdate (boardId, data) {
+  return admin
+    .database()
+    .ref(`projects/${boardId}`)
+    .set(data)
+}
+
+function isRedBucketLabel (boardId, labelId) {
+  return admin
+    .database()
+    .ref(`projects/${boardId}/labels/${labelId}`)
+    .once('value')
+    .then(snap => snap.exists())
+}
+
+function addDefect (boardId, cardId, labelId) {
+  return admin
+    .database()
+    .ref(`projects/${boardId}/defects`)
+    .push({
+      cardId,
+      labelId
+    })
+}
+
+function removeDefect (boardId, cardId, labelId) {
+  return admin
+    .database()
+    .ref(`projects/${boardId}/defects`)
+    .once('value')
+    .then(snapshot => {
+      const defects = snapshot.val()
+
+      const defectId = Object.keys(defects).find(defectId => {
+        const defect = defects[defectId]
+
+        return defect.cardId === cardId && defect.labelId === labelId
+      })
+
+      return snapshot.ref.child(defectId).remove()
+    })
+}
+
+module.exports = {
+  addDefect,
+  createOrUpdate,
+  isRedBucketLabel,
+  isTracked,
+  removeDefect
+}
---
+++ functions/hook.js
@@ -0,0 +1,43 @@
+const functions = require('firebase-functions')
+const { projects } = require('./db')
+
+function processAction (action, res) {
+  const {
+    data: { board, card, label },
+    type
+  } = action
+
+  return projects.isTracked(board.id)
+  .then(tracked => {
+    if (!tracked) return
+
+    switch (type) {
+      case 'addLabelToCard':
+        return projects
+          .isRedBucketLabel(board.id, label.id)
+          .then(redBucket => redBucket
+            ? projects.addDefect(board.id, card.id, label.id)
+            : null
+          )
+      case 'removeLabelFromCard':
+        return projects
+          .isRedBucketLabel(board.id, label.id)
+          .then(redBucket => redBucket
+            ? projects.removeDefect(board.id, card.id, label.id)
+            : null
+          )
+    }
+  })
+  .then(() => res.send('Done'))
+}
+
+module.exports = functions.https.onRequest((req, res) => {
+  switch (req.method) {
+    case 'POST':
+      processAction(req.body.action, res)
+      break
+    default:
+      // Don't send any data in case it's a HEAD request
+      res.status(200).end()
+  }
+})
