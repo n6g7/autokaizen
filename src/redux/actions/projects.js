1a4814833dd75bd915d28c03f435527991746fa1
Nathan Gaberel <nathang@theodo.co.uk>
2017-08-19 19:50:18 +0100

    Add project creation actions.

--- src/redux/actions/projects.js
+++ src/redux/actions/projects.js
@@ -1,6 +1,27 @@
 export const types = {
+  CREATE_PROJECT: {
+    REQUEST: 'CREATE_PROJECT.REQUEST',
+    SUCCESS: 'CREATE_PROJECT.SUCCESS',
+    FAILURE: 'CREATE_PROJECT.FAILURE'
+  },
   SYNC_PROJECTS: 'SYNC_PROJECTS'
 }
+
+export const createProject = (boardId, name, currentSprint) => ({
+  type: types.CREATE_PROJECT.REQUEST,
+  boardId,
+  name,
+  currentSprint
+})
+
+export const createProjectSuccess = () => ({
+  type: types.CREATE_PROJECT.SUCCESS
+})
+
+export const createProjectFailure = error => ({
+  type: types.CREATE_PROJECT.FAILURE,
+  error
+})
 
 export const syncProjects = projects => ({
   type: types.SYNC_PROJECTS,
--- src/redux/reducers/projects.js
+++ src/redux/reducers/projects.js
@@ -1,7 +1,8 @@
 import { types } from '@actions/projects'
 
 const initialState = {
-  list: {}
+  list: {},
+  loading: false
 }
 
 export default function projects (state = initialState, action = {}) {
@@ -10,6 +11,17 @@ export default function projects (state = initialState, action = {}) {
       return {
         ...state,
         list: action.projects
+      }
+    case types.CREATE_PROJECT.REQUEST:
+      return {
+        ...state,
+        loading: true
+      }
+    case types.CREATE_PROJECT.SUCCESS:
+    case types.CREATE_PROJECT.FAILURE:
+      return {
+        ...state,
+        loading: false
       }
     default:
       return state
