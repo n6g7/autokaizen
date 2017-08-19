85d821eaca5d7e9e0611c91c3578864160f86fb7
Nathan Gaberel <nathang@theodo.co.uk>
2017-08-19 19:45:22 +0100

    Add Trello client.

---
+++ src/services/trello.js
@@ -0,0 +1,26 @@
+export default {
+  authorize () {
+    return new Promise((resolve, reject) => {
+      window.Trello.authorize({
+        name: 'Auto Kaizen',
+        persist: true,
+        type: 'popup',
+        success: resolve,
+        error: reject
+      })
+    })
+  },
+  createWebhook (idModel, callbackURL) {
+    return new Promise((resolve, reject) => {
+      window.Trello.post(
+        '/webhooks',
+        {
+          idModel,
+          callbackURL
+        },
+        resolve,
+        reject
+      )
+    })
+  }
+}
