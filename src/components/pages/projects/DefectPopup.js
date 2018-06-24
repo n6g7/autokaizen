bcc833d24706684879a3fe1844c67c486320fb68
Nathan Gaberel <nathan@gnab.fr>
2018-06-24 22:23:00 +0100

    Add DefectPopup page.

---
+++ src/components/pages/projects/DefectPopup.js
@@ -0,0 +1,95 @@
+import React, { Fragment, PureComponent } from 'react'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+import ReactDOM from 'react-dom'
+import styled from 'styled-components'
+import { DateTime } from 'luxon'
+
+import { TicketNumber } from '@atoms'
+import { Category, Date, Sprint } from '@molecules'
+import { defectSelector } from '@selectors/defects'
+import { projectSelector } from '@selectors/projects'
+import { Popup } from '@templates'
+
+const Project = styled.p`
+  color: ${p => p.theme.text.darker};
+  font-size: 1.25em;
+  font-weight: bolder;
+  margin: 0;
+
+  span {
+    font-weight: normal;
+    opacity: 0.8;
+  }
+`
+
+const UserStory = styled.p`
+  font-weight: lighter;
+  margin: ${p => 3 * p.theme.spacing}px 0 ${p => 2 * p.theme.spacing}px;
+`
+
+const SubTitle = styled.h4`
+  font-size: 1.5em;
+  margin: 0 0 ${p => 3 * p.theme.spacing}px;
+`
+
+const Textarea = styled.textarea`
+  background: transparent;
+  border: none;
+  border-bottom: 2px solid ${p => p.theme.background.lighter};
+  color: inherit;
+  font-family: inherit;
+  font-size: inherit;
+  font-weight: lighter;
+  outline: none;
+  padding: 0 0 ${p => p.theme.spacing}px;
+  resize: vertical;
+  width: 100%;
+`
+
+class DefectPopup extends PureComponent {
+  static propTypes = {
+    defect: PropTypes.object.isRequired,
+    project: PropTypes.object.isRequired
+  }
+
+  renderMain () {
+    const { defect, project } = this.props
+
+    return <Fragment>
+      <Project>
+        {project.client && <span>{project.client} / </span>}
+        {project.name}
+      </Project>
+      <TicketNumber large>{ defect.cardNumber }</TicketNumber>
+      <UserStory>{defect.userStory}</UserStory>
+      <Date date={DateTime.fromMillis(defect.creation)} dark />
+      <Sprint number={defect.sprint} dark />
+      <Category labelId={defect.labelId} dark />
+    </Fragment>
+  }
+
+  renderFooter () {
+    return <Fragment>
+      <SubTitle>Notes</SubTitle>
+      <Textarea />
+    </Fragment>
+  }
+
+  render () {
+    return ReactDOM.createPortal(
+      <Popup
+        main={this.renderMain()}
+        footer={this.renderFooter()}
+      />,
+      document.getElementById('popup')
+    )
+  }
+}
+
+const mapStateToProps = (state, ownProps) => ({
+  defect: defectSelector(state, ownProps),
+  project: projectSelector(state, ownProps)
+})
+
+export default connect(mapStateToProps)(DefectPopup)
--- src/components/pages/projects/index.js
+++ src/components/pages/projects/index.js
@@ -2,6 +2,7 @@ import React, { PureComponent } from 'react'
 import { Route, Switch } from 'react-router-dom'
 
 import Add from './Add'
+import DefectPopup from './DefectPopup'
 import Defects from './Defects'
 import Details from './Details'
 
@@ -9,11 +10,14 @@ class Projects extends PureComponent {
   render () {
     const { match } = this.props
 
-    return <Switch>
-      <Route exact path={`${match.url}/add`} component={Add} />
-      <Route path={`${match.url}/:projectId/defects`} component={Defects} />
-      <Route path={`${match.url}/:projectId`} component={Details} />
-    </Switch>
+    return [
+      <Switch>
+        <Route exact path={`${match.url}/add`} component={Add} />
+        <Route path={`${match.url}/:projectId/defects`} component={Defects} />
+        <Route path={`${match.url}/:projectId`} component={Details} />
+      </Switch>,
+      <Route exact path={`${match.url}/:projectId/defects/:defectId`} component={DefectPopup} />
+    ]
   }
 }
 
