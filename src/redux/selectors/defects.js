c033e536afde7dd89b5510010347ea84ab2b3fb8
Nathan Gaberel <nathan@gnab.fr>
2018-06-24 22:20:47 +0100

    Add DefectSelector.

--- src/redux/selectors/defects.js
+++ src/redux/selectors/defects.js
@@ -1,8 +1,16 @@
 import { createSelector } from 'reselect'
 import { keyComparator, objectToArray } from './base'
+import { routerDefectIdSelector } from './router'
 
 const rawDefectsSelector = state => state.defects.list
 const labelFiltersSelector = state => state.tracker.filters.labels
+const propsDefectIdSelector = (state, props) => props.match.params.defectId
+
+const defectIdSelector = createSelector(
+  propsDefectIdSelector,
+  routerDefectIdSelector,
+  (propsId, routerId) => propsId || routerId
+)
 
 export const defectsSelector = createSelector(
   rawDefectsSelector,
@@ -14,4 +22,9 @@ export const filteredDefectsSelector = createSelector(
   (defects, labelFilters) => labelFilters.length === 0
     ? defects
     : defects.filter(d => labelFilters.includes(d.labelId))
+)
+
+export const defectSelector = createSelector(
+  defectsSelector, defectIdSelector,
+  (defects, defectId) => defects.find(d => d.id === defectId)
 )
--- src/redux/selectors/router.js
+++ src/redux/selectors/router.js
@@ -2,6 +2,14 @@ import { createSelector } from 'reselect'
 
 const pathnameSelector = state => state.router.location.pathname
 
+export const routerDefectIdSelector = createSelector(
+  pathnameSelector,
+  pathname => {
+    const match = /^\/projects\/([0-9a-f]+)\/defects\/([0-9a-f]+)(\/|$)/.exec(pathname)
+    return match ? match[2] : null
+  }
+)
+
 export const routerProjectIdSelector = createSelector(
   pathnameSelector,
   pathname => {
